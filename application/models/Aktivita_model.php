64c1b21d15766110a71d4392853ca4f802c8fa94
mpuk <puk.matej@gmail.com>
2017-06-24 23:27:24 +0200

    created models Znamka, Aktivita

---
+++ application/models/Aktivita_model.php
@@ -0,0 +1,8 @@
+class Znamka_model extends CI_Model {
+
+        public $id;
+        public $nazov;
+        public $popis;
+        public $maximum;
+
+}
---
+++ application/models/Znamka_model.php
@@ -0,0 +1,9 @@
+class Znamka_model extends CI_Model {
+
+        public $id;
+        public $meno;
+        public $priezvisko;
+        public $datum;
+        public $Aktivita_id;
+
+}
